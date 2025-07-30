7401e3b046a60667e6457a6ada45df4b8b4ed14b
huyshelby <qhuyofficial@gmail.com>
2025-07-30 22:47:18 +0700

    cmmne

---
+++ client/ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "bo-ne", // Tên ứng dụng của bạn
+      script: "npm", // Chương trình để chạy
+      args: "run start", // Đối số truyền cho chương trình 'npm'
+    },
+  ],
+};
